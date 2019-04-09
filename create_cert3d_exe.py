f1706ab5863f9f3a0d4892cd97d629be0c72e6dd
Tim Kostka <kostka@gmail.com>
2019-04-09 14:03:39 -0400

    added EXE creation script

---
+++ create_cert3d_exe.py
@@ -0,0 +1,45 @@
+"""
+This file creates a pyinstaller EXE file.
+
+"""
+
+import os
+import shutil
+import datetime
+
+####################
+# START OF OPTIONS #
+####################
+
+##################
+# END OF OPTIONS #
+##################
+
+# path of the EXE to create
+exe_path = os.path.abspath("./dist/cert3d_gui.exe")
+
+# delete existing EXE
+if os.path.isfile(exe_path):
+    os.remove(exe_path)
+
+# create the EXE
+arguments = [
+    "pyinstaller",
+    "-n cert3d_gui",
+    "--clean",
+    "--noconsole",
+    "--onefile",
+#    "-i scterm.ico",
+#    "--add-data scterm.ico;.",
+#    "--add-data sct_logo.png;.",
+    "cert3d.py",
+]
+os.system(" ".join(arguments))
+
+# if file wasn't created, something went wrong
+if not os.path.isfile(exe_path):
+    print("\nERROR: could not create EXE file")
+    exit(1)
+
+# report success
+print("\nEXE created at %s" % exe_path)
